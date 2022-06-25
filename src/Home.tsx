3e784257927e261ddff6257bf015a193747d7f59
famouscat <3142362556@qq.com>
2022-06-25 14:32:24 +0800

    update: 支持选择大学

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,57 +1,136 @@
-import { AppBar, Paper, SpeedDial, Toolbar } from "@mui/material";
-import { PostsQuery, usePostsQuery } from "./generated/graphql";
 import {
-    Add as AddIcon,
-} from '@mui/icons-material';
+  AppBar,
+  Avatar,
+  Dialog,
+  DialogTitle,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  SpeedDial,
+  Toolbar,
+} from "@mui/material";
+import {
+  PostsQuery,
+  usePostsQuery,
+  useUniversitiesQuery,
+} from "./generated/graphql";
+import { Add as AddIcon } from "@mui/icons-material";
 import { Virtuoso } from "react-virtuoso";
 import PostItem from "./components/PostItem";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
+import { useUniversitySelector } from "./hooks";
+import { useState } from "react";
+
+function UniversitySelector() {
+  const { universityState, setUniversityState } = useUniversitySelector();
+  const { data, error, loading } = useUniversitiesQuery();
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const universities = data?.universities.edges.map((i) => i.node);
+
+  const handleListItemClick = (index: number) => {
+    if (!universities) {
+      setOpen(false);
+      return;
+    }
+    const u = universities[index];
+    setUniversityState({
+      id: u?.id ?? "",
+      name: u?.name ?? "",
+      logoUrl: u?.logoUrl ?? "",
+    });
+    setOpen(false);
+  };
+
+  if (error) return <div>Error</div>;
+  if (loading) return <div>loading</div>;
+
+  return (
+    <>
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle>选择大学</DialogTitle>
+        <List>
+          {universities?.map((i, index) => (
+            <ListItem
+              button
+              onClick={() => handleListItemClick(index)}
+              key={index}
+            >
+              <ListItemAvatar>
+                <Avatar alt={i?.name ?? "avatar"} src={i?.logoUrl ?? ""} />
+              </ListItemAvatar>
+              <ListItemText primary={i?.name ?? "N/A"} />
+            </ListItem>
+          ))}
+        </List>
+      </Dialog>
+      <IconButton onClick={handleOpen}>
+        <Avatar
+          sx={{ bgcolor: 'white' }}
+          alt={universityState ? universityState.name : "N/A"}
+          src={universityState ? universityState.logoUrl : "N/A"}
+        />
+      </IconButton>
+    </>
+  );
+}
 
 export default function Home() {
-    const navigate = useNavigate();
-    const { loading, error, data, fetchMore } = usePostsQuery();
-    
-    if (loading) return <div> Loading... </div>
-    if(error) return <div>Something went wrong...</div>
+  const navigate = useNavigate();
+  const { loading, error, data, fetchMore } = usePostsQuery();
 
-    const pageInfo = data?.postsWithRelay.pageInfo
+  if (loading) return <div> Loading... </div>;
+  if (error) return <div>Something went wrong...</div>;
 
-    const itemContent = (index: number, postsQuery: PostsQuery | undefined) => {
-        const post = postsQuery?.postsWithRelay.edges[index].node
-        return <PostItem post={post} />
-    }
+  const pageInfo = data?.postsWithRelay.pageInfo;
 
-    return (
-        <>
-        <AppBar position="static">
-            <Toolbar>
-                <SearchBar />
-            </Toolbar>
-        </AppBar>
-        <Paper sx={{backgroundColor: '#eee', flexGrow: 1, height: '100%' }}>
-            <SpeedDial
-                onClick={ () => {
-                    navigate('/create-post')
-                }}
-                ariaLabel="SpeedDial"
-                sx={{ position: 'fixed', bottom: 'calc(56px + 16px)', right: 16 }}
-                icon={<AddIcon />} />
-            <Virtuoso
-                style={{ height: 'calc(100vh - 56px)', flexGrow: 1 }}
-                totalCount={data?.postsWithRelay.edges.length ?? 0}
-                itemContent={(index) => itemContent(index, data)}
-                endReached={index => {
-                    fetchMore({
-                        variables: {
-                            after: pageInfo?.endCursor,
-                            first: 10,
-                        }
-                    })
-                }}
-            >
-            </Virtuoso>
-        </Paper>
-        </>
-    )
+  const itemContent = (index: number, postsQuery: PostsQuery | undefined) => {
+    const post = postsQuery?.postsWithRelay.edges[index].node;
+    return <PostItem post={post} />;
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <UniversitySelector />
+          <SearchBar />
+        </Toolbar>
+      </AppBar>
+      <Paper sx={{ backgroundColor: "#eee", flexGrow: 1, height: "100%" }}>
+        <SpeedDial
+          onClick={() => {
+            navigate("/create-post");
+          }}
+          ariaLabel="SpeedDial"
+          sx={{ position: "fixed", bottom: "calc(56px + 16px)", right: 16 }}
+          icon={<AddIcon />}
+        />
+        <Virtuoso
+          style={{ height: "calc(100vh - 56px)", flexGrow: 1 }}
+          totalCount={data?.postsWithRelay.edges.length ?? 0}
+          itemContent={(index) => itemContent(index, data)}
+          endReached={(index) => {
+            fetchMore({
+              variables: {
+                after: pageInfo?.endCursor,
+                first: 10,
+              },
+            });
+          }}
+        ></Virtuoso>
+      </Paper>
+    </>
+  );
 }
--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,25 +1,53 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export function useLoginStatus() {
-    const [state, setState] = useState<string | undefined | null>('')
+  const [state, setState] = useState<string | undefined | null>("");
 
+  useEffect(() => {
+    setState(localStorage.getItem("token"));
+  }, [state]);
 
-    useEffect(() => {
-        setState(localStorage.getItem('token'))
-    }, [state])
+  useEffect(() => {
+    if (!state) return;
+    localStorage.setItem("token", state);
+  }, [state]);
 
-    useEffect(() => {
-        if (!state) return
-        localStorage.setItem('token', state)
-    }, [state])
+  return {
+    loginState: state,
+    setLoginState: (value: string | undefined) => {
+      setState(value);
+    },
+    removeLoginState: () => {
+      localStorage.removeItem("token");
+      setState(null);
+    },
+  };
+}
 
+export interface UniversitySelectorProps {
+  id: string;
+  logoUrl: string;
+  name: string;
+}
 
-    return {
-        loginState: state, setLoginState: (value: string | undefined) => {
-            setState(value)
-        }, removeLoginState: () => {
-            localStorage.removeItem('token')
-            setState(null)
-        }
-    }
+export function useUniversitySelector() {
+  const [state, setState] = useState<
+    UniversitySelectorProps | undefined | null
+  >(
+    JSON.parse(
+      localStorage.getItem("university") as string
+    ) as unknown as UniversitySelectorProps
+  );
+
+  return {
+    universityState: state,
+    setUniversityState: (value: UniversitySelectorProps) => {
+      localStorage.setItem("university", JSON.stringify(value));
+      setState(value);
+    },
+    removeUniversityState: () => {
+      localStorage.removeItem("university");
+      setState(null);
+    },
+  };
 }
