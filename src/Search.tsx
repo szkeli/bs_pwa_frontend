3c9e5500c4d8fa8c33c0c115fe057f614493654f
famouscat <onismaa@gmail.com>
2022-07-02 16:50:30 +0800

    update: css

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,5 +1,33 @@
+import {
+  Search as SearchBase,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "./components/SearchBar";
+import { Search as SearchIcon } from "@mui/icons-material";
+import { AppBar, Toolbar } from "@mui/material";
+
 export default function Search() {
-    return (
-        <div>dsadsa</div>
-    )
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
+}
+
+function SearchBar() {
+  return (
+    <AppBar position="static" elevation={0}>
+      <Toolbar>
+        <SearchBase>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="search..."
+            inputProps={{ "aria-label": "search" }}
+          />
+        </SearchBase>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 import { alpha, InputBase, styled } from "@mui/material";
 import { Search as SearchIcon } from '@mui/icons-material';
 
-const Search = styled('div')(({theme}) => ({
+export const Search = styled('div')(({theme}) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
@@ -17,7 +17,7 @@ const Search = styled('div')(({theme}) => ({
     }
 }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
+export const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 1, 1, 0),
@@ -30,7 +30,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     }
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+export const SearchIconWrapper = styled('div')(({ theme }) => ({
     padding: theme.spacing(0 ,2),
     height: '100%',
     position: 'absolute',
